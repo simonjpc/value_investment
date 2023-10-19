a9fa3ef9f3ce96cb8002ae75e568a17e9572cb06
simonpcpc <simon.palma@axionable.com>
2023-10-19 23:55:57 +0200

    test to make sqlalchemy engine work in multiprocessing

--- valuation/pipeline.py
+++ valuation/pipeline.py
@@ -1,7 +1,9 @@
 import sys
 import pandas as pd
+import multiprocessing
 import concurrent.futures
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, exc
+from sqlalchemy.pool import QueuePool
 from valuation.extraction import get_income_stmt_info, get_balance_sheet_info
 from valuation.utils import drop_df_cols, add_suffix_to_cols, dict_to_df, batch_tickers
 from valuation.data_injection import Injector
@@ -20,8 +22,10 @@ logging.basicConfig(
 )
 log = logging.getLogger(__name__)
 
-def define_engine()
-def single_ticker_pipeline(ticker, connection):
+injector = Injector()
+engine = create_engine(injector.db_uri, poolclass=QueuePool, pool_size=10, max_overflow=20)
+
+def single_ticker_pipeline(ticker):
     income_stmt = get_income_stmt_info(ticker=ticker, period="quarter", limit=120)
     balance_sheet = get_balance_sheet_info(ticker=ticker, period="quarter", limit=120)
     log.info("balance sheet & income stmt data loaded.")
@@ -51,43 +55,53 @@ def single_ticker_pipeline(ticker, connection):
     financial_info = financial_info.reset_index()
     log.info("concatenation succeeded.")
 
-    injector = Injector()
     try:
-        injector.df_dump(df=financial_info, connection=connection)
+        injector.df_dump(df=financial_info, engine=engine)
     except:
         raise("data dump failed")
     log.info("data dump successful.")
+    engine.dispose()
     return True
 
 if __name__ == "__main__":
-    #start = time.time()
     # create table if it does not exist
-    injector = Injector()
+    log.info("very beginning")
     dataloader = DataLoader()
-    engine = create_engine(injector.db_uri)
-    connection = engine.connect()
-    injector.create_table(
-        FINANCIAL_STMT_DUMP_QUERY.format(table_name=FINANCIAL_STMT_TABLE_NAME),
-        connection,
-    )
-    # multiprocessing
+    injector = Injector()
+    log.info("dataloader & injector constructors called")
+    # batches creation
     tickers = dataloader.load_tickers_from_txt(TICKERS_PATH)
     ticker_batches = batch_tickers(tickers=tickers, batch_size=100)
-    # We execute this per batch considering the limit of the api
-    for idx, batch in enumerate(ticker_batches):
-        log.info(f"Starting batch {idx + 1} with {len(batch)} tickers...")
-        executor = concurrent.futures.ProcessPoolExecutor()
-        dumping_futures = [
-            executor.submit(single_ticker_pipeline, args=(ticker, connection)) for ticker in batch
-        ]
-        for future in concurrent.futures.as_completed(dumping_futures):
-            dumping_flag = future.result()
-        log.info(f"batch {idx + 1} executed")
-        log.info(f"waiting 1 min...")
-        time.sleep(60)
-        log.info(f"Restarting")
-    connection.close()
-    engine.dispose()
+    log.info("tickers loaded and batches created")
+    
+    connection = engine.connect()
+    log.info("engine defined and first connection created")
+    try:
+        injector.create_table(
+            FINANCIAL_STMT_DUMP_QUERY.format(table_name=FINANCIAL_STMT_TABLE_NAME),
+            connection,
+        )
+        
+        # Multiprocessing
+        # We execute this per batch considering the limit of the api
+        for idx, batch in enumerate(ticker_batches[:2]):
+            log.info(f"Starting batch {idx + 1} with {len(batch)} tickers...")
 
-    #end = time.time()
-    #log.info(f"execution time: {end - start}")
+            with concurrent.futures.ProcessPoolExecutor() as executor:
+                dumping_futures = [
+                    executor.submit(single_ticker_pipeline, ticker) for ticker in batch
+                ]
+                for future in concurrent.futures.as_completed(dumping_futures):
+                    dumping_flag = future.result()
+
+            log.info(f"batch {idx + 1} executed")
+            log.info(f"waiting 1 min...")
+            time.sleep(61)
+            log.info(f"Restarting")
+
+    except exc.SQLAlchemyError as e:
+        print(f"An error occurred: {e}")
+
+    finally:
+        connection.close()
+        engine.dispose()
