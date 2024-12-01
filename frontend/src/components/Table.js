db7852915d9f6b663118a8b824040e514fb9d82b
Simon Palma <simon@MacBook-Pro-de-Simon.local>
2024-12-01 17:50:47 +0100

    fixing cols headers in table

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -33,25 +33,34 @@ const Table = ({ data }) => {
 };
 
 const styles = {
-  tableContainer: {
-    maxHeight: '400px',
-    overflowY: 'scroll',
-    border: '1px solid #ccc',
-    padding: '10px',
-  },
-  table: {
-    width: '100%',
-    borderCollapse: 'collapse',
-  },
-  header: {
-    border: '1px solid #ccc',
-    padding: '8px',
-    backgroundColor: '#f2f2f2',
-  },
-  cell: {
-    border: '1px solid #ccc',
-    padding: '8px',
-  },
-};
+    tableContainer: {
+      maxHeight: '400px',
+      overflowY: 'scroll',
+      border: '0px solid #ccc',
+      padding: '0px',
+    },
+    table: {
+      width: '100%',
+      borderCollapse: 'collapse',
+    },
+    headerContainer: {
+      position: 'sticky',
+      top: 0,
+      backgroundColor: '#f2f2f2',
+      zIndex: 1,
+    },
+    header: {
+      border: '0px solid #ccc',
+      padding: '10px',
+      backgroundColor: '#f2f2f2',
+      position: 'sticky',
+      top: 0,
+      zIndex: 1,
+    },
+    cell: {
+      border: '1px solid #ccc',
+      padding: '8px',
+    },
+  };
 
 export default Table;
